d41d7151ad3b756610d5605ec591ec03022b2293
Ryan Barth <ryan@ryportal.com>
2017-01-27 19:00:54 -0800

    refine to showcase html lanuage list issue

--- Main.elm
+++ Main.elm
@@ -14,6 +14,14 @@ main = Markdown.toHtmlWith myOptions [] """
 <p>Hello world!</p>
 ```
 
+```xml
+<p>Hello world!</p>
+```
+
+```javascript
+{ number: 4 }
+```
+
 # Inline code:
 Lorem ipsum dolor `{ key: "value" }` sit amet, consectetur adipiscing elit. Vestibulum sit amet magna in velit lacinia suscipit at vel nunc. Sed gravida nibh vel urna faucibus cursus. Phasellus sit amet tortor orci. Pellentesque elementum est enim, vitae blandit sapien pellentesque sed. Morbi sollicitudin et orci ut sodales. Phasellus ac augue molestie, tristique dui eget, dapibus erat. Donec quis tempor dolor. Vivamus sit amet erat blandit, tempor leo eu, ultricies neque. Curabitur tempus gravida malesuada. Ut porta lectus at sem efficitur rhoncus. Sed posuere sit amet neque sed dignissim. Nulla posuere, nunc et sodales eleifend, orci massa scelerisque justo, at rutrum nibh sem ut magna. Aenean sapien nisl, ornare in placerat ac, convallis eget massa.
 
